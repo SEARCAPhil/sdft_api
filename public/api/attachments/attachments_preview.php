5956d4bed40d5763f4d52b92c65f489a7b8eca4a
jkga <johnkennethgibasabella@gmail.com>
2019-06-17 17:39:59 +0800

    Added attachment preview and auth api

---
+++ public/api/attachments/attachments_preview.php
@@ -0,0 +1,175 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+/*@ini_set('zlib.output_compression', 'Off');
+@ini_set('output_buffering', 'Off');
+@ini_set('output_handler', '');
+@apache_setenv('no-gzip', 1);*/
+session_start();
+
+
+
+use SDFT\Baskets;
+use SDFT\Token;
+use SDFT\Attachments;
+use SDFT\Activities;
+use SDFT\Baskets\Collaborators;
+
+
+require_once('../../../vendor/autoload.php');
+require_once('../../../config/database.php');
+
+
+$response=array('status'=>300);
+
+function download($db,$id){
+		
+
+	try{
+		$db->beginTransaction();
+		$id=htmlentities(htmlspecialchars($id));
+		$file_exists=0;
+		$returnFile='File not found!This will automatically close after 5 seconds.<script>setTimeout(function(){window.close();},5000);</script>';
+			$attach_sql="SELECT attachments.* from attachments left join basket on basket.id=attachments.basket_id where attachments.id=:id";
+			$attach_statement=$db->prepare($attach_sql);
+			$attach_statement->bindParam(':id',$id);
+			$attach_statement->execute();
+			if($row=$attach_statement->fetch(PDO::FETCH_OBJ)){
+
+					$basket_id = $row->basket_id;
+
+					if(!is_null($row->original_copy_id)){
+						//original copy informatoin
+						$attach_sql2="SELECT attachments.* from attachments left join basket on basket.id=attachments.basket_id where attachments.id=:id";
+						$attach_statement2=$db->prepare($attach_sql2);
+						$attach_statement2->bindParam(':id',$row->original_copy_id);
+						$attach_statement2->execute();
+
+						if($row2=$attach_statement2->fetch(PDO::FETCH_OBJ)){
+							
+							//origina basket where file is located on the server
+							$basket_id = $row2->basket_id;
+						}
+					}
+          
+          $server_file = $_SERVER['DOCUMENT_ROOT'].'/sdft_api/public/uploads/'.$basket_id.'/'.$row->filename;
+          $server_extension = pathinfo($server_file, PATHINFO_EXTENSION);
+					if(file_exists($server_file)){ 
+            $file_exists = 1;
+            //$returnFile.=readfile ($_SERVER['DOCUMENT_ROOT'].'/sdft_api/public/uploads/'.$basket_id.'/'.$row->filename);
+            # view per mime
+            if(in_array($server_extension,['png', 'jpg', 'jpeg', 'bmp'])) {
+              $returnFile='<img src="../../../public/uploads/'.$basket_id.'/'.$row->filename.'"/>';
+            }
+            
+            if($server_extension === 'pdf') {
+              $returnFile =header('Content-type: application/pdf');
+              $returnFile.=header('Content-Disposition: inline; filename="' . $filename . '"');
+              $returnFile.=header('Content-Transfer-Encoding: binary');
+              $returnFile.=header('Content-Length: ' . filesize($server_file));
+              $returnFile.=header('Accept-Ranges: bytes');
+              $returnFile.=ob_clean();
+              $returnFile.=flush();
+              $returnFile.=@readfile ($server_file);
+              return $returnFile;
+            }
+          }
+
+          
+			}
+
+		
+      $db->commit();
+      
+      if($file_exists){
+				echo $returnFile;
+			}else{
+				echo $returnFile;
+      }
+      
+		}catch(PDOException $e){$db->rollback(); echo $e->getMessage();}
+
+
+
+}
+
+
+//block if no token in param
+if(!isset($_GET['token'])) exit;
+
+$token=htmlentities(htmlspecialchars($_GET['token']));
+
+
+
+//Block if token is empty
+if(empty($token)) exit;
+
+
+
+//Validate token
+$token_class=new Token();
+$__identity=$token_class->get_token($db,$token);
+
+
+
+$ip=$_SERVER['REMOTE_ADDR'];
+//get ip address
+
+
+
+if(isset($__identity->id)){
+	//check current ip address if the same with identity IP
+//	if(!filter_var($ip,FILTER_VALIDATE_IP)==TRUE) exit;
+
+//	if(filter_var($ip,FILTER_VALIDATE_IP)!=$__identity->ip_address) exit;
+
+}
+
+
+
+//must contain files
+if(!isset($_GET['id']))	exit;
+
+$id=(int) htmlentities(htmlspecialchars($_GET['id']));
+
+
+//get parent basket
+$attachments=new Attachments();
+$parent=$attachments->details($db,$id);
+$basket_id=$parent[0]->basket_id;
+$file_name=$parent[0]->original_filename;
+
+
+	/*--------------------------------
+	| Prevent unauthorized access
+	|--------------------------------*/
+	//get basket information
+	$collaborators=new Collaborators();
+
+	$basket_collaborators=($collaborators->get_collaborators($db,$basket_id,0));
+
+
+	$collaborators_array=array();
+
+	if(isset($basket_collaborators[0]->uid)){
+
+			for ($i=0; $i <count($basket_collaborators); $i++) { 
+				
+				array_push($collaborators_array, $basket_collaborators[$i]->uid);
+
+			}
+		
+	}
+
+
+	#allow them to view if they are collaborators
+	if(in_array($__identity->uid,$collaborators_array)){
+
+		download($db,$id);
+
+	}else{
+		echo 'File not found!This will automatically close after 5 seconds.<script>setTimeout(function(){window.close();},5000);</script>';
+	}
+
+
+
+?>
